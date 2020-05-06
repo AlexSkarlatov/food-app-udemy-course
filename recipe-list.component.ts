5ae4a006fa60e79d73792fa2c8838739f7ae7688
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-05-06 16:57:24 -0400

    compare this code from instructor to my own code
    compare this code from instructor to my own code and determine the differences on github comparison tool. refer to the branch of the same name except with fail appended to it for further details on the nature of the failure.

--- recipe-list.component.ts
+++ recipe-list.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('pie test', 'describing the test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('me test', 'can i see my face', 'https://www.linkedin.com/in/alexander-skarlatov-8b4199105/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BzyNbR10KTZuWhd5ptjG13w%3D%3D')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
-//https://www.linkedin.com/in/alexander-skarlatov-8b4199105/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BzyNbR10KTZuWhd5ptjG13w%3D%3D
 
   constructor() { }
 
-
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+   this.recipeWasSelected.emit(recipe);
   }
 
 }
