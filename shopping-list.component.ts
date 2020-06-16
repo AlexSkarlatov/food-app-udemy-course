13b6a01a8f11fc70bf30dcbd0eb20856f7980be8
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-06-16 17:02:54 -0400

    Add files via upload

--- shopping-list.component.ts
+++ shopping-list.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Ingredient } from '../../shared/ingredient.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService }  from './shopping-list.service';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
-  ingredients = [
-    new Ingredient('apple', 5),
-    new Ingredient('tomato', 10)
-
-  ];
-
-
-  constructor() { }
+  ingredients: Ingredient[];
+// private slService1: ShoppingListService1
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe(
+      (ings: Ingredient[]) => {
+        this.ingredients = ings;
+      }
+    );
   }
 
+  // onIngredientAdded(ingredient: Ingredient) {
+  //   //this.ingredients.push(ingredient);
+  // }
 }
