b917e3aef3c4d023ff021023ac4b96aa81ec5709
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-06-16 16:49:46 -0400

    Add files via upload

--- header.component.ts
+++ header.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @Output() featureSelectedEvent = new EventEmitter<string>();
 
-  constructor() { }
-
-  ngOnInit() {
+  onSelect(feature: string) {
+    this.featureSelectedEvent.emit(feature);
   }
-
 }
--- recipe-list.component.ts
+++ recipe-list.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('pie test', 'describing the test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('me test', 'can i see my face', 'https://www.linkedin.com/in/alexander-skarlatov-8b4199105/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BzyNbR10KTZuWhd5ptjG13w%3D%3D')
-  ];
-//https://www.linkedin.com/in/alexander-skarlatov-8b4199105/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BzyNbR10KTZuWhd5ptjG13w%3D%3D
 
-  constructor() { }
+  recipes: Recipe[];
+
+  constructor(private recService: RecipeService) { }
+
 
 
   ngOnInit() {
+    this.recipes = this.recService.getRecipes();
   }
+
+
 
 }
